1ba5e2812f4f96921c12386cf1f49c41feaab1b5
yjsong-PC\yjsong <syj9302@gmail.com>
2018-04-07 12:09:55 +0900

    /api/foos 추가

---
+++ spring-security-rest/src/main/java/com/yjsong/springsecurityrest/VO/Foo.java
@@ -0,0 +1,11 @@
+package com.yjsong.springsecurityrest.VO;
+
+import lombok.Data;
+
+@Data
+public class Foo {
+	
+	private int id;
+	private String name;
+	
+}
--- spring-security-rest/src/main/java/com/yjsong/springsecurityrest/controller/HomeController.java
+++ spring-security-rest/src/main/java/com/yjsong/springsecurityrest/controller/HomeController.java
@@ -1,16 +1,25 @@
 package com.yjsong.springsecurityrest.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yjsong.springsecurityrest.VO.Foo;
 
 @RestController
 @RequestMapping("/api")
 public class HomeController {
 	
 	@GetMapping("/foos")
-	public String foos() {
-		return "test";
+	public ArrayList<Foo> foos() {
+		ArrayList<Foo> fooList = new ArrayList<Foo>();
+		Foo foo = new Foo();
+		foo.setId(0);
+		foo.setName("JbidXc");
+		fooList.add(foo);
+		return fooList;
     }
 	
 }
